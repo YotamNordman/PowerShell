928631800e0c0117e6c05a6abb3a7bf60774e494
haxxx42 <yotam.nordman@gmail.com>
2017-07-12 21:25:35 +0300

    Adding some DSC for tests

--- ScriptBlueprint1.0.ps1
+++ ScriptBlueprint1.0.ps1
@@ -151,7 +151,8 @@ Set-StrictMode -Version 1
 # Test-NetConnection   ->  Is like Telnet to check a port
 #endregion
 #endregion
-function Login-Faceook {
+#region Facebook Login
+function Login-Facebook {
 # Remember to give creds as param
 # $Credential = Get-Credential
 # Login-Faceook $Credential
@@ -169,6 +170,7 @@ function Login-Faceook {
   $r = Invoke-WebRequest -Uri $form.Action -WebSession $fb -Method POST -Body $form.Fields
   $r
 }
+#endregion Facebook Login
 #region DSC First Tests
 Configuration MyFirstDSC
 {
@@ -231,7 +233,114 @@ MyFirstDSC
 #notepad .\MyFirstDSC\Web2012R2.mof
 
 Start-DscConfiguration  -Verbose -Path 'C:\PSTest\MyFirstDSC\MyFirstDSC\' -Force -Wait -ComputerName 'DESKTOP-COBF18G'
+Get-DscConfigurationStatus
+Get-DscConfiguration
 #Stop-DscConfiguration 
 #Enable-PSRemoting
 #Enable-PSRemoting -SkipNetworkProfileCheck
 #endregion DSC First Tests
+#region DSC New Domain
+configuration NewDomain             
+{             
+   param             
+    (             
+        [Parameter(Mandatory)]             
+        [pscredential]$safemodeAdministratorCred,             
+        [Parameter(Mandatory)]            
+        [pscredential]$domainCred            
+    )             
+            
+    Import-DscResource -ModuleName xActiveDirectory            
+            
+    Node $AllNodes.Where{$_.Role -eq "Primary DC"}.Nodename             
+    {             
+            
+        LocalConfigurationManager            
+        {            
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true            
+        }            
+            
+        File ADFiles            
+        {            
+            DestinationPath = 'N:\NTDS'            
+            Type = 'Directory'            
+            Ensure = 'Present'            
+        }            
+                    
+        WindowsFeature ADDSInstall             
+        {             
+            Ensure = "Present"             
+            Name = "AD-Domain-Services"             
+        }            
+            
+        # Optional GUI tools            
+        WindowsFeature ADDSTools            
+        {             
+            Ensure = "Present"             
+            Name = "RSAT-ADDS"             
+        }            
+            
+        # No slash at end of folder paths            
+        xADDomain FirstDS             
+        {             
+            DomainName = $Node.DomainName             
+            DomainAdministratorCredential = $domainCred             
+            SafemodeAdministratorPassword = $safemodeAdministratorCred            
+            DatabasePath = 'N:\NTDS'            
+            LogPath = 'N:\NTDS'            
+            DependsOn = "[WindowsFeature]ADDSInstall","[File]ADFiles"            
+        }            
+            
+    }             
+}            
+            
+# Configuration Data for AD              
+$ConfigData = @{             
+    AllNodes = @(             
+        @{             
+            Nodename = "localhost"             
+            Role = "Primary DC"             
+            DomainName = "alpineskihouse.com"             
+            RetryCount = 20              
+            RetryIntervalSec = 30            
+            PsDscAllowPlainTextPassword = $true            
+        }            
+    )             
+}             
+            
+NewDomain -ConfigurationData $ConfigData `
+    -safemodeAdministratorCred (Get-Credential -UserName '(Password Only)' `
+        -Message "New Domain Safe Mode Administrator Password") `
+    -domainCred (Get-Credential -UserName alpineskihouse\administrator `
+        -Message "New Domain Admin Credential")            
+            
+# Make sure that LCM is set to continue configuration after reboot            
+Set-DSCLocalConfigurationManager -Path .\NewDomain –Verbose            
+            
+# Build the domain            
+Start-DscConfiguration -Wait -Force -Path .\NewDomain -Verbose    
+#endregion DSC New Domain
+#region DSC Install Chrome
+Configuration Sample_InstallChromeBrowser
+{
+    param
+    (
+    [Parameter(Mandatory)]
+    $Language,
+        
+    [Parameter(Mandatory)]
+    $LocalPath
+        
+    )
+    
+    Import-DscResource -module xChrome
+    
+    MSFT_xChrome chrome
+    {
+    Language = $Language
+    LocalPath = $LocalPath
+    }
+}
+#endregion DSC Install Chrome
